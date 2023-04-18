7b2ff26a22a62fd330fe75dd00acf40c48d3145c
Ryan Park <ryrypark@gmail.com>
2023-04-18 16:32:14 -0700

    second code review

--- wordfinder.py
+++ wordfinder.py
@@ -1,20 +1,45 @@
 from random import choice
 
-def read_words_to_list(file):
-    my_file = open(file, "r")
-    words = my_file.read()
-    word_list = words.split("\n")
-    return word_list
+#TODO: encapsulate method inside of class
+#TODO: use comprehension to make word list
+#TODO: add __repr__
+
 
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
     def __init__(self, path_to_file):
-        self.word_list = read_words_to_list(path_to_file)
+        """Creates a word list from a file and prints the length of the list"""
+
+        self.word_list = self.read_words_to_list(path_to_file)
+
         print(f"{len(self.word_list)} words read")
 
+    def read_words_to_list(self, file):
+        """takes in and reads a file and turns it into a list of strings"""
+
+        my_file = open(file, "r")
+
+        return [word.strip() for word in my_file]
+
     def random(self):
+        """chooses a random word from the list"""
+
         random_word = choice(self.word_list)
+        
         return random_word
 
-wf = WordFinder("gistfile1.txt")
-print(wf.random())
+
+class SpecialWordFinder(WordFinder):
+    """Special Word Finder: finds random words from a dictionary
+    but filters out blank lines and lines that start with #"""
+    # def __init__(self, path_to_file):
+    #     """Creates a filtered word list from a file and prints the length of the list"""
+    #     super().__init__(path_to_file)
+    #     self.word_list = [word for word in self.word_list if not (word.startswith("#") or word == "")]
+
+
+
+# wf = SpecialWordFinder("gistfile1.txt")
+# print(wf.word_list)
+# print(wf.random())
