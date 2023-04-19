105ed884b427b48c14a1fbdc612c0faf700290a9
Hannah Nguyen <hnguyen160801@gmail.com>
2023-04-18 20:05:51 -0400

    final

--- wordfinder.py
+++ wordfinder.py
@@ -11,35 +11,44 @@ class WordFinder:
     def __init__(self, path_to_file):
         """Creates a word list from a file and prints the length of the list"""
 
-        self.word_list = self.read_words_to_list(path_to_file)
+        my_file = open(path_to_file, "r")
+
+        self.word_list = self.read_words_to_list(my_file)
 
         print(f"{len(self.word_list)} words read")
 
     def read_words_to_list(self, file):
         """takes in and reads a file and turns it into a list of strings"""
 
-        my_file = open(file, "r")
-
-        return [word.strip() for word in my_file]
+        return [word.strip() for word in file]
 
     def random(self):
         """chooses a random word from the list"""
 
         random_word = choice(self.word_list)
-        
+
         return random_word
+
+    def __repr__(self):
+        return f"<WordFinder path_to_file={self.path_to_file}"
 
 
 class SpecialWordFinder(WordFinder):
     """Special Word Finder: finds random words from a dictionary
     but filters out blank lines and lines that start with #"""
-    # def __init__(self, path_to_file):
-    #     """Creates a filtered word list from a file and prints the length of the list"""
-    #     super().__init__(path_to_file)
-    #     self.word_list = [word for word in self.word_list if not (word.startswith("#") or word == "")]
+
+    def read_words_to_list(self, file):
+        """takes in and reads a file and turns it into a list of strings, accounting
+        for lines that start with "#" and empty lines."""
+
+        # return [word.strip() for word in file if not (word.startswith("#") or len(word.strip()) == 0)]
+        return [word for word in super().read_words_to_list(file) if not (word.startswith("#") or len(word) == 0)]
+
+    def __repr__(self):
+        return f"<Special WordFinder path_to_file={self.path_to_file}"
 
 
 
-# wf = SpecialWordFinder("gistfile1.txt")
-# print(wf.word_list)
-# print(wf.random())
+wf = SpecialWordFinder("complex.txt")
+print(wf.word_list)
+print(wf.random())
